daa328dda3c6ca3b997922ae51ac41c24cd13c70
samcat437 <sdrees12@gmail.com>
2022-10-31 15:59:49 +0000

    added stripe variables in checkout views, added in gitpod workspace, refreshing

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -10,8 +10,6 @@ var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
-var card = elements.create('card', {style: style});
-
 var style = {
     base: {
         color: '#000',
@@ -27,5 +25,21 @@ var style = {
         iconColor: '#dc3545'
     }
 };
-
+var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Handle realtime validation
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
