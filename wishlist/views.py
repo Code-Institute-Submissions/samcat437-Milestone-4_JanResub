039f4de934b3078326aab6c214f5409ee7a4983f
samcat437 <sdrees12@gmail.com>
2022-11-02 22:27:07 +0000

    added logic to wishlist view, product not adding to wishlist dictionary

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import UserProfile
 
+from products.models import Product
 @login_required
 def wishlist(request):
     """ A view to show logged in user's wishlist """
@@ -20,12 +22,14 @@ def add_to_wishlist(request, item_id):
     """ A view to add an item to the user's wishlist """
 
     product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     wishlist = request.session.get('wishlist', {})
 
-    wishlist[item_id] = quantity
-    messages.success(request, f'Added {product.name} to your wishlist')
+    if item_id in list(wishlist.keys()):
+        messages.success(request, f'This product already exists in your wishlist.')
+    else:
+        messages.success(request, f'Added {product.name} to your wishlist')
 
     request.session['wishlist'] = wishlist
+    print(request.session['wishlist'])
     return redirect(redirect_url)
