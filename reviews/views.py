0b961129905d38fba879c22b531630fb8edd132c
samcat437 <sdrees12@gmail.com>
2022-11-04 18:29:55 +0000

    added add basic add review functionality

--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile, Product
+from .forms import ReviewForm
 
 
+@login_required
 def reviews(request):
     """ A view to return the reviews on a given product"""
 
@@ -11,6 +13,21 @@ def reviews(request):
 
 @login_required
 def add_review(request):
-    """ A view to add an item to the user's wishlist """
+    """ A view to add an review to the user's wishlist """
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, request.FILES)
+        if form.is_valid():
+            review = form.save()
+            messages.success(request, 'Successfully added review!')
+            return redirect(reverse('reviews'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ReviewForm()
 
-    return render(request, 'reviews/add_review.html')
+    template = 'reviews/add_review.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
