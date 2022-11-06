3c199a6f5531588d26309d761ee78f19716f2eea
samcat437 <sdrees12@gmail.com>
2022-11-06 14:12:16 +0000

    form customisation on reveiws/forms.py

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,12 +1,25 @@
 from django import forms
-from .models import Product, Reviews
+from .models import Reviews
 
 
 class ReviewForm(forms.ModelForm):
 
     class Meta:
         model = Reviews
-        fields = '__all__'
+        exclude = ('user',)
 
     def __init__(self, *args, **kwargs):
+        """ 
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
         super().__init__(*args, **kwargs)
+        placeholders = {
+            'product': 'Select Product',
+            'review': 'Write your review here',
+         }
+
+        self.fields['product'].widget.attrs['autofocus'] = True
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
