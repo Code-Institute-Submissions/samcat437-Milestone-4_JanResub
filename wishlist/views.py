3be45f19d027a3838f40204995b307bcc69980f2
samcat437 <sdrees12@gmail.com>
2022-11-07 16:35:54 +0000

    work on add_to_wishlist, not yet functional

--- wishlist/views.py
+++ wishlist/views.py
@@ -20,20 +20,18 @@ def add_to_wishlist(request, item_id):
     user = get_object_or_404(UserProfile, user=request.user) 
     product = get_object_or_404(Product, pk=item_id) 
     redirect_url = request.POST.get('redirect_url')
-    in_wishlist = WishlistItem.objects.filter(product=product, user=request.user).exists()
+    in_wishlist = Wishlist.objects.filter(product=product, user=user).exists()
     wishlist_exists = Wishlist.objects.filter(user=user).exists()
 
     if in_wishlist:
         messages.info(request, f'This product already exists in your wishlist.')
     else:
         if wishlist_exists:
-            WishlistItem.product.add(product)
-            WishlistItem.save()
+            Wishlist.save(product)
             messages.success(request, f'Added {product.name} to your wishlist')
         else:
             user_wishlist = Wishlist.objects.create(product=product, user=user)
-            WishlistItem.product.add(product)
-            WishlistItem.save()
+            Wishlist.save(product)
             messages.success(request, f'Added {product.name} to your wishlist')
 
     return redirect(redirect_url)
