d63f2ed57263a0627dbdf13645a6a21b9159442b
samcat437 <sdrees12@gmail.com>
2022-11-10 13:20:23 +0000

    removed secret key and set debug

--- vintage_sound/settings.py
+++ vintage_sound/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&9!)n@q8vz9n1ak8$y^tlew0^*)p9jqc$&bx=ju(xk6j@**b+-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['milestone-4-vintage-sound.herokuapp.com', 'localhost']
 
